fb8b008af80ed404209f467b4b7640199d432bd0
weizjajj <weizhongjie.wzj@antgroup.com>
2025-02-14 10:54:34 +0800

    add case

--- examples/sample_standard_app/intelligence/test/test_demo_agent.py
+++ examples/sample_standard_app/intelligence/test/test_demo_agent.py
@@ -1,11 +1,12 @@
 # !/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
+import queue
 # @Time    : 2024/4/1 14:32
 # @Author  : wangchongshi
 # @Email   : wangchongshi.wcs@antgroup.com
 # @FileName: test_rag_agent.py
 import unittest
+from threading import Thread
 
 from agentuniverse.agent.agent import Agent
 from agentuniverse.agent.agent_manager import AgentManager
@@ -21,14 +22,31 @@ class DemoAgentTest(unittest.TestCase):
     def setUp(self) -> None:
         AgentUniverse().start(config_path='../../config/config.toml')
 
-    def test_demo_agent(self):
-        """Test demo rag agent."""
-        instance: Agent = AgentManager().get_instance_obj('demo_agent')
-        output_object: OutputObject = instance.run(input='分析下巴菲特减持比亚迪的原因')
-        res_info = f"\nDemo agent execution result is :\n"
-        res_info += output_object.get_data('output')
-        print(res_info)
+    # def test_demo_agent(self):
+    #     """Test demo rag agent."""
+    #     instance: Agent = AgentManager().get_instance_obj('demo_agent')
+    #     output_object: OutputObject = instance.run(input='分析下巴菲特减持比亚迪的原因')
+    #     res_info = f"\nDemo agent execution result is :\n"
+    #     res_info += output_object.get_data('output')
+    #     print(res_info)
 
+    def read_output(self,output_stream:queue.Queue):
+        while True:
+            try:
+                res = output_stream.get()
+                if res == '{"type": "EOF"}':
+                    break
+                print(res)
+            except queue.Empty:
+                break
+
+    def test_demo_agent_stream(self):
+        output_stream = queue.Queue(10)
+        instance: Agent = AgentManager().get_instance_obj('demo_agent')
+        Thread(target=self.read_output, args=(output_stream,)).start()
+        print("=======================================================")
+        result = instance.run(input='分析下巴菲特减持比亚迪的原因', output_stream=output_stream)
+        print(result)
 
 if __name__ == '__main__':
     unittest.main()
