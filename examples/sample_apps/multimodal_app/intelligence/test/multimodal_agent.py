f0a848109fe980007e5ca93dbafa9f89866c063b
wangchongshi <wangchongshi.wcs@antgroup.com>
2025-02-19 16:12:29 +0800

    feat: add multimodal agent examples.

--- examples/sample_apps/multimodal_app/intelligence/test/multimodal_agent.py
+++ examples/sample_apps/multimodal_app/intelligence/test/multimodal_agent.py
@@ -12,7 +12,7 @@ from agentuniverse.agent.agent_manager import AgentManager
 AgentUniverse().start(config_path='../../config/config.toml', core_mode=True)
 
 
-def chat():
+def chat_with_images():
     instance: Agent = AgentManager().get_instance_obj('multimodal_agent')
     output_object = instance.run(input="图中描绘的是什么景象?", image_urls=[
         'https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241022/emyrja/dog_and_girl.jpeg'])
@@ -25,5 +25,15 @@ def chat():
     print(res_info)
 
 
+def chat_with_audio():
+    instance: Agent = AgentManager().get_instance_obj('multimodal_agent')
+    output_object = instance.run(input='帮我完成音频里的任务内容？',
+                                 audio_url='https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20240916/kvkadk/%E6%8E%A8%E8%8D%90%E4%B9%A6.wav')
+    res_info = f"\nagent运行结果是:\n"
+    res_info += output_object.get_data('output')
+    print(res_info)
+
+
 if __name__ == '__main__':
-    chat()
+    # chat_with_images()
+    chat_with_audio()
