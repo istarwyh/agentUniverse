0f6cd58c98d9b568a7827c8c295f10bbc10f63e9
wangchongshi <wangchongshi.wcs@antgroup.com>
2025-02-19 16:28:31 +0800

    feat: add multimodal agent examples.

--- examples/sample_apps/multimodal_app/intelligence/test/multimodal_agent.py
+++ examples/sample_apps/multimodal_app/intelligence/test/multimodal_agent.py
@@ -14,7 +14,7 @@ AgentUniverse().start(config_path='../../config/config.toml', core_mode=True)
 
 def chat_with_images():
     instance: Agent = AgentManager().get_instance_obj('multimodal_agent')
-    output_object = instance.run(input="图中描绘的是什么景象?", image_urls=[
+    output_object = instance.run(input="图中描绘的是什么景象?", session_id='test_image', image_urls=[
         'https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20241022/emyrja/dog_and_girl.jpeg'])
     res_info = f"\n图中描绘的景象是:\n"
     res_info += output_object.get_data('output')
@@ -27,7 +27,7 @@ def chat_with_images():
 
 def chat_with_audio():
     instance: Agent = AgentManager().get_instance_obj('multimodal_agent')
-    output_object = instance.run(input='帮我完成音频里的任务内容？',
+    output_object = instance.run(input='帮我完成音频里的任务内容？', session_id='test_audio',
                                  audio_url='https://help-static-aliyun-doc.aliyuncs.com/file-manage-files/zh-CN/20240916/kvkadk/%E6%8E%A8%E8%8D%90%E4%B9%A6.wav')
     res_info = f"\nagent运行结果是:\n"
     res_info += output_object.get_data('output')
